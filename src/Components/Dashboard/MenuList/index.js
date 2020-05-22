97e00273fbbddcdf00d159037d760098fdf847bc
Elang Indra <tlurie@student.sierracollege.edu>
2020-05-22 10:26:08 +0700

    finish sidebar

--- src/Components/Dashboard/MenuList/index.js
+++ src/Components/Dashboard/MenuList/index.js
@@ -1,32 +1,57 @@
 import React from 'react';
 
 import './style.scss';
+import { Icon } from '@iconify/react';
+
+import home from '@iconify/icons-mdi/home';
+import settings from '@iconify/icons-mdi/settings-outline';
+import mySurvey from '@iconify/icons-mdi/check-circle-outline';
+import listSurvey from '@iconify/icons-mdi/clipboard-text-outline';
 
 const MenuList = () => {
   const menus = [
     {
       title: 'home',
+      icon: home,
       link: '#',
+      active: true,
     },
     {
-      title: 'manage',
+      title: 'my survey',
+      icon: mySurvey,
       link: '#',
     },
     {
       title: 'view',
+      icon: listSurvey,
       link: '#',
     },
     {
       title: 'settings',
+      icon: settings,
       link: '#',
     },
   ];
 
-  const listMenu = menus.map((menu, i) => (
-    <li key={i}>
-      <a href={menu.link}>{menu.title}</a>
-    </li>
-  ));
+  const listMenu = menus.map((menu, i) => {
+    if (menu.active) {
+      return (
+        <li key={i}>
+          <a class='active' href={menu.link}>
+            <Icon icon={menu.icon} />
+          </a>
+        </li>
+      );
+    } else {
+      return (
+        <li key={i}>
+          <a href={menu.link}>
+            <Icon icon={menu.icon} />
+          </a>
+        </li>
+      );
+    }
+  });
 
   return <ul className='menu-list'>{listMenu}</ul>;
 };
