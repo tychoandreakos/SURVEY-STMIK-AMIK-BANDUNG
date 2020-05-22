9c67dd6dfab6890ca0d5ee7be1b526eb661ac088
Elang Indra <tlurie@student.sierracollege.edu>
2020-05-22 08:52:38 +0700

    create a sidebar component

---
+++ src/Components/Dashboard/Sidebar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div id='sidebar'>
+      <h3>Halo Dunia</h3>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/Components/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Sidebar from './Sidebar';
+
+function Dashboard() {
+  return (
+    <div id='dashboard-survey'>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Dashboard;
