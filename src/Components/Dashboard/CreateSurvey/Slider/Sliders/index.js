66b61da8a1769176e1918ff2da7c6421fb9b58ca
Elang Indra <tlurie@student.sierracollege.edu>
2020-07-21 15:49:31 +0700

    finishing the panel of create survey

--- src/Components/Dashboard/CreateSurvey/Slider/Sliders/index.js
+++ src/Components/Dashboard/CreateSurvey/Slider/Sliders/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const SlidersCreate = () => {
+import './style.scss';
+
+import { Icon } from '@iconify/react';
+import checkIcon from '@iconify/icons-mdi/check';
+
+const SlidersCreate = ({ icon, title, active }) => {
+
+    let check;
+    const sliders = ['sliders-create'];
+    if (active) {
+        check = (
+            <div className="active-sliders">
+                <Icon icon={checkIcon} />
+            </div>
+        )
+        sliders.push('active-create-sliders')
+    }
+
+    const slidersClass = sliders.join(' ')
+
+
     return (
-        <div className="sliders-create">
-            <h3>Its Work</h3>
+        <div className={slidersClass}>
+            <div className="panel-sliders">
+                <div className="icon">
+                    <Icon icon={icon} />
+                </div>
+                <h3>{title}</h3>
+                {check}
+            </div>
         </div>
     )
 }
--- src/Components/Dashboard/CreateSurvey/Slider/index.js
+++ src/Components/Dashboard/CreateSurvey/Slider/index.js
@@ -2,20 +2,44 @@ import React from 'react';
 
 import SlidersCreate from './Sliders';
 
-import 'style.scss';
+import clumsy from '@iconify/icons-mdi/1up';
+import money from '@iconify/icons-mdi/money'
+import love from '@iconify/icons-mdi/love'
+
+import './style.scss';
 
 const SliderCreate = () => {
-    const slider = 3;
+    const slider = [
+        {
+            icon: clumsy,
+            title: 'clumsy',
+            active: false,
+        },
+        {
+            icon: money,
+            title: 'money',
+            active: true,
+        },
+        {
+            icon: love,
+            title: 'love',
+            active: false,
+        },
+    ];
     const slidersComponent = [];
-    for (let i = 0; i < slider; i++) {
-        slidersComponent.push(<SlidersCreate key={i} />)
-    }
+    
+    slider.forEach((item, i) => {
+        const itemToPush = <SlidersCreate key={i} icon={item.icon} title={item.title} active={item.active} />;
 
+        slidersComponent.push(itemToPush)
+    });
 
     return (
         <div className="slider">
-            <h3>category</h3>
-            {slidersComponent}
+            <h3 className="title-sliders">category</h3>
+            <div className="sliders">
+                {slidersComponent}
+            </div>
         </div>
     )
 }
