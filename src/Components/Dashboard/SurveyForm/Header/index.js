0104ed903bec1ae723d17c389d057e1403dae5ea
Elang Indra <tlurie@student.sierracollege.edu>
2020-07-25 10:20:15 +0700

    finishing the header component

--- src/Components/Dashboard/SurveyForm/Header/index.js
+++ src/Components/Dashboard/SurveyForm/Header/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Icon from '@iconify/react'
+import ArrowLeft from '@iconify/icons-mdi/chevron-left'
+import Eye from '@iconify/icons-mdi/eye';
+
 import UserAcccount from '../../UserAccount';
 import BreadcrumbHeader from '../Breadcrumbs';
 
@@ -13,9 +17,19 @@ const SurveyHeaderForm = () => {
                 <UserAcccount />
             </div>
             <div className="bottom-header">
-                <div className="back-btn"></div>
-                <div className="title"></div>
-                <div className="btn-result"></div>
+                <div className="back-btn">
+                    <div className="icon">
+                        <Icon icon={ArrowLeft} />
+                    </div>
+                    <span>form builder</span>
+                </div>
+                <h2 className="title">most love music bands</h2>
+                <div className="btn-result">
+                    <div className="icon">
+                        <Icon icon={Eye} />
+                    </div>
+                    <span>View Result</span>
+                </div>
             </div>
         </div >
     )
