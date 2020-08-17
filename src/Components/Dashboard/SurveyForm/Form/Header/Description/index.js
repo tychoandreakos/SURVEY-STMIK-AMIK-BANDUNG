e13aa44ffe6c54aa2cb4777ee389d176e304de8d
Elang Indra <tlurie@student.sierracollege.edu>
2020-08-17 07:11:52 +0700

    finishing the title header compnoent

--- src/Components/Dashboard/SurveyForm/Form/Header/Description/index.js
+++ src/Components/Dashboard/SurveyForm/Form/Header/Description/index.js
@@ -6,6 +6,7 @@ import './style.scss';
 const DescriptionHeader = () => {
     const [pageDesciption, editPageDescription] = ['page description', 'edit page description']
     const [pageTitle, setpageTitle] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nesciunt praesentium aperiam ex sapiente accusantium!')
+    const [pageDesc, setPageDesc] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quia accusamus adipisci, magni officiis dolore!')
     const initialState = {
         edit: false,
         title: pageDesciption
@@ -15,13 +16,19 @@ const DescriptionHeader = () => {
     const { edit, title } = state;
 
 
-    const titleRef = useRef('');
+    const titleRef = useRef({});
+    const descRef = useRef({});
     const [titleHeight, setTitleHeight] = useState('')
+    const [descHeight, setDescHeight] = useState('')
     useEffect(() => {
         if (titleRef.current !== null) {
-            setTitleHeight(titleRef.current.clientHeight)
+            setTitleHeight(titleRef.current.clientHeight + 15)
         }
-    }, [edit, pageTitle])
+
+        if (descRef.current !== null) {
+            setDescHeight(descRef.current.clientHeight + 15)
+        }
+    }, [edit])
 
 
     function reducer(state, action) {
@@ -58,7 +65,23 @@ const DescriptionHeader = () => {
 
     const titleHandlerKeyPress = (e) => {
         if (e.charCode === 13) {
-            setTitleHeight(titleHeight + 35)
+            e.preventDefault();
+            dispatch({
+                type: DESCRIPTION_HEADER.SAVE
+            })
+        }
+    }
+
+    const descHandler = (e) => {
+        setPageDesc(e.target.value)
+    }
+
+    const descHandlerKeyPress = (e) => {
+        if (e.charCode === 13) {
+            e.preventDefault();
+            dispatch({
+                type: DESCRIPTION_HEADER.SAVE
+            })
         }
     }
 
@@ -84,11 +107,34 @@ const DescriptionHeader = () => {
             'edited-desc'
         ]
         btnEl = <button className="finish" onClick={saveHandler} >SAVE</button>;
-        headerPageEl = <textarea onKeyPress={titleHandlerKeyPress} style={{ height: `${titleHeight}px` }} type="text" onChange={titleHandler} value={pageTitle} className="page-title-input" placeholder="Please insert your page title" />
-
+        headerPageEl = (
+            <>
+                <textarea
+                    onKeyPress={titleHandlerKeyPress}
+                    style={{ height: `${titleHeight}px` }}
+                    type="text" onChange={titleHandler}
+                    value={pageTitle}
+                    className="page-title-input"
+                    placeholder="Please insert your page title"
+                />
+                <textarea
+                    className="desc-survey-master edited-desc"
+                    onKeyPress={descHandlerKeyPress}
+                    style={{ height: `${descHeight}px` }}
+                    type="text" onChange={descHandler}
+                    value={pageDesc}
+                    placeholder="Please insert your page description"
+                />
+            </>
+        )
     } else {
         btnEl = <button className="edit" onClick={editHandler} >EDIT</button>
-        headerPageEl = <h3 ref={titleRef}>{pageTitle}</h3>
+        headerPageEl = (
+            <>
+                <h3 ref={titleRef}>{pageTitle}</h3>
+                <span ref={descRef} className="desc-survey-master">{pageDesc}</span>
+            </>
+        )
     }
 
     const pollClass = [classDescription, classDesc, classTitle];
@@ -104,7 +150,7 @@ const DescriptionHeader = () => {
                 </div>
             </div>
             {headerPageEl}
-            <span className={descClass}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quia accusamus adipisci, magni officiis dolore!</span>
+
         </div>
     )
 }
