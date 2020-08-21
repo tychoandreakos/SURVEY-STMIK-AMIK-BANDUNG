5c145119445aa2adab695a6b4df5cc6f99d6e87b
Elang Indra <tlurie@student.sierracollege.edu>
2020-08-21 10:57:54 +0700

    finishing add the image

--- src/Components/Dashboard/SurveyForm/Form/Header/LogoButton/index.js
+++ src/Components/Dashboard/SurveyForm/Form/Header/LogoButton/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Icon from '@iconify/react';
 
 import DefaultIcon from '@iconify/icons-mdi/plus';
@@ -7,13 +7,54 @@ import './style.scss';
 
 const LogoButton = (props) => {
 
+    const [image, setImage] = useState();
+    const [actualImage, setActualImage] = useState();
     const { title = "Add a Logo", icon = DefaultIcon } = props;
+    const inputFileRef = useRef();
+
+    useEffect(() => {
+        if (image && (image.size < 4718592)) {
+            let reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = () => {
+                setActualImage(reader.result);
+            };
+        }
+    }, [image])
+
+    const readFile = (e) => {
+        const imageFiles = e.target.files[0];
+        setImage(imageFiles);
+    }
+
+    const uploadHandler = () => {
+        const elem = inputFileRef.current;
+        elem.click()
+    }
+
+    let renderEl;
+    if (actualImage) {
+        renderEl = (
+            <img
+                onClick={uploadHandler}
+                className="logo"
+                src={actualImage}
+            />
+        )
+    } else {
+        renderEl = (
+            <button onClick={uploadHandler} className="btn-add-header">
+                <Icon className="icon-add-header" icon={icon} />
+                <span>{title}</span>
+            </button>
+        )
+    }
 
     return (
-        <button className="btn-add-header">
-            <Icon className="icon-add-header" icon={icon} />
-            <span>{title}</span>
-        </button>
+        <div className="upload-logo">
+            {renderEl}
+            <input type="file" onChange={readFile} hidden ref={inputFileRef} />
+        </div>
     )
 }
 
