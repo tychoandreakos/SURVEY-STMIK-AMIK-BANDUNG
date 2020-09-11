d6640abcead217a6270b255070323b81f53fb6ba
tychoandreakos <elangindra15@gmail.com>
2020-09-11 14:52:16 +0000

    finishing loader in logoButton

--- src/Components/Dashboard/SurveyForm/Form/Header/LogoButton/index.js
+++ src/Components/Dashboard/SurveyForm/Form/Header/LogoButton/index.js
@@ -1,9 +1,15 @@
-import React, { useRef, useState, useEffect, useCallback } from "react";
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+} from "react";
 import { connect } from "react-redux";
 
 import Icon from "@iconify/react";
-
 import DefaultIcon from "@iconify/icons-mdi/plus";
+import Loader from "react-loading";
 
 import "./style.scss";
 import { processingLogo } from "../../../../../../Store/redux/action";
@@ -11,8 +17,37 @@ import { processingLogo } from "../../../../../../Store/redux/action";
 const LogoButton = (props) => {
   const [image, setImage] = useState();
   const [actualImage, setActualImage] = useState();
+  const [loader, setLoader] = useState(false);
   const { title = "Add a Logo", icon = DefaultIcon, processingLogo } = props;
   const inputFileRef = useRef();
+
+  const loaderRef = useRef({
+    type: "bubbles",
+    color: "#5661b6",
+    height: "8%",
+    width: "8%",
+  });
+
+  const loaderState = useMemo(() => {
+    const { type, color, height, width } = loaderRef.current;
+    return {
+      type,
+      color,
+      height,
+      width,
+    };
+  }, [loaderRef.current]);
+
+  const loaderEl = (
+    <div className='loading'>
+      <Loader
+        type={loaderState.type}
+        color={loaderState.color}
+        height={loaderState.height}
+        width={loaderState.width}
+      />
+    </div>
+  );
 
   const processingLogoCallback = useCallback(() => {
     if (image && image.size < 4718592) {
@@ -23,12 +58,21 @@ const LogoButton = (props) => {
       };
 
       processingLogo();
+      setLoader(true);
     }
   }, [image, processingLogo]);
 
   useEffect(() => {
     processingLogoCallback();
   }, [processingLogoCallback]);
+
+  useEffect(() => {
+    if (actualImage && actualImage.length > 0) {
+      setTimeout(() => {
+        setLoader(false);
+      }, 1500);
+    }
+  }, [actualImage]);
 
   const readFile = (e) => {
     const imageFiles = e.target.files[0];
@@ -41,7 +85,11 @@ const LogoButton = (props) => {
   };
 
   let renderEl;
-  if (actualImage) {
+  if (loader) {
+    renderEl = loaderEl;
+  }
+
+  if (!loader && actualImage) {
     renderEl = (
       <img
         alt='logo'
@@ -50,7 +98,9 @@ const LogoButton = (props) => {
         src={actualImage}
       />
     );
-  } else {
+  }
+
+  if (!loader && !actualImage) {
     renderEl = (
       <button onClick={uploadHandler} className='btn-add-header'>
         <Icon className='icon-add-header' icon={icon} />
