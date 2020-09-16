b9514fc824fa87b0cb946b2fcc27e503d474605e
tychoandreakos <elangindra15@gmail.com>
2020-09-16 17:28:11 +0000

    Adding validation to login & signup

--- src/Components/Util/AuthWrapper/index.js
+++ src/Components/Util/AuthWrapper/index.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 
 import Icon from "@iconify/react";
 import FingerPrint from "@iconify/icons-mdi/fingerprint";
 import ButtonSubmit from "../ButtonSubmit";
 
 import "./style.scss";
-import { useMemo } from "react";
 
 const AuthWrapper = (props) => {
   const { children, footer, btnTitle, term = false } = props;
+  const [validation, setValidation] = useState(false);
   const termRender = useMemo(() => {
     if (term) {
       return (
@@ -20,6 +20,26 @@ const AuthWrapper = (props) => {
       );
     }
   }, [term]);
+
+  const validationRender = useMemo(() => {
+    if (validation) {
+      return (
+        <div className='validation'>
+          <ul>
+            <li>Username / password Wrong!</li>
+            <li>Please enter 8 character</li>
+          </ul>
+        </div>
+      );
+    }
+
+    return [];
+  }, [validation]);
+
+  const validationHandler = () => {
+    setValidation(!validation);
+  };
+
   return (
     <div className='auth'>
       <div className='auth-wrapper'>
@@ -28,6 +48,7 @@ const AuthWrapper = (props) => {
         </div>
         <div className='form'>
           {children}
+          {validationRender}
           <ButtonSubmit title={btnTitle} />
           {termRender}
         </div>
