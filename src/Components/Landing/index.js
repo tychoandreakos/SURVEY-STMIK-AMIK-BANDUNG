37c3fb5ddd250f307a4a2380fda9c7f29f0ede84
tychoandreakos <elangindra15@gmail.com>
2020-09-17 02:21:39 +0000

    Fixed redirect

--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Landing = () => {
-  return <h1>Its working</h1>;
+import { HOME_DASHBOARD } from "../../util/route";
+
+const Landing = (props) => {
+  useEffect(() => {
+    props.history.push(HOME_DASHBOARD);
+  }, []);
+  return <h1></h1>;
 };
 
 export default Landing;
